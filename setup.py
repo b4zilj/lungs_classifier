9b10b9eefa0cc683bf41de0c7e9243a80ff121df
basil <basilj412@gmail.com>
2025-07-25 12:53:27 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lungs_classifier',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    description='A small python package for CNN app',
+    author='b4zilj',
+    author_email='basilj412@gmail.com',
+)
